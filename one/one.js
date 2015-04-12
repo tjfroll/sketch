ae4aa8e781e950ece3634121e49933e38be9475c
Tim Froehlich <tjfroll@gmail.com>
2015-04-12 11:03:58 -0400

    Might actually work this time

--- one/one.js
+++ one/one.js
@@ -9,7 +9,7 @@ var freq;
 var amp;
 
 function setup() {
- size(900, 980);
+ createCanvas(900, 980);
  background(0);
   
  x = 0;
@@ -29,38 +29,37 @@ function draw() {
   currentLine = (0.4) * 10;
 
   if (increaseAmp) {
-    currentAmp = currentAmp - .01;
+    currentAmp -= .01;
   } else {
-    currentAmp = currentAmp + .01;
+    currentAmp += .01;
   }
    
-  if (currentAmp == 1) {
+  if (currentAmp === 1) {
     increaseAmp = true;
-  } else if (currentAmp == 0) {
+  } else if (currentAmp === 0) {
     increaseAmp = false;
   }
    
   if (increaseRed) {
-    red = red + 1;
+    red += 1;
   } else {
-    red = red - 1;
+    red -= 1;
   }
-  if (red == 255) {
+  if (red === 255) {
     increaseRed = true;
   } else if (red == 0) {
     increaseRed = false;
   }
      
-  turn = turn + 1;
+  turn += 1;
    
-  for(var deg = 0; deg < 360; deg = deg + 8)
-  {
+  for (var deg = 0; deg < 360; deg += 8) {
     makeSquiggle(deg, currentAmp, turn, red);
   }
 }
  
 function makeSquiggle(deg, wobble, turn, reddish) {
-  pushMatrix();
+  push();
   translate(width / 2, height / 2);
 
   rotate( radians(deg + (turn * .1)) );
@@ -68,7 +67,7 @@ function makeSquiggle(deg, wobble, turn, reddish) {
   beginShape();
   fill(255, deg, 0, 10);
   
-  for(var i = 0; i < 200; i++) {
+  for (var i = 0; i < 20; i++) {
     stroke(reddish, 0, 0, 30);
      
     x = i * currentLine;
@@ -78,5 +77,5 @@ function makeSquiggle(deg, wobble, turn, reddish) {
   }
 
   endShape();
-  popMatrix();
+  pop();
 }
