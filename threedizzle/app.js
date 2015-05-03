7fb78bb7983915eb89257018e53c5950a5bef801
Tim Froehlich <tjfroll@gmail.com>
2015-05-02 23:03:34 -0400

    Back to normalcy a bit, cubes fly at you in what is ostensibly a sane manner

--- threedizzle/app.js
+++ threedizzle/app.js
@@ -1,17 +1,30 @@
 /*global THREE*/
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, -10, 50 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .1, 50000);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+color1 = '#6F256F';
+color2 = '#983352';
+color3 = '#609732';
+color4 = '#91A437';
+
+color5 = '#226666';
+color6 = '#2E4172';
+color7 = '#AA8439';
+color8 = '#AA6C39';
+
+colors = [color1, color2, color3, color4, color5, color6, color7, color8];
 
 var cubes = [];
 
 addCube();
 
 console.log(cubes[0]);
+console.log(scene);
 
 camera.position.z = 5;
 
@@ -19,15 +32,17 @@ var frameCount = 0;
 function render() {
   requestAnimationFrame( render );
   frameCount += 1;
-  if(frameCount === 30) {
-    console.log(cubes[0]);
+  if(frameCount === 1) {
     frameCount = 0;
     addCube();
   }
   cubes.forEach(function (cube) {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    cube.position.z -= 0.25;
+    cube.rotation.x += .1;
+    cube.rotation.y += .1;
+    cube.position.z += 100;
+    if(cube.position.z > -10) {
+      scene.remove(cube);
+    }
   });
   renderer.render( scene, camera );
 }
@@ -36,20 +51,23 @@ function addCube() {
   var width = 1 + random(150);
   var height = 1 + random(300);
   var depth = 1 + random(500);
-  var color = 'rgb(' + random(250) + ',' + random(250) + ',' + random(250) + ')';
+  var color = colors[random(8)];
   var geometry = new THREE.BoxGeometry( width, height, depth);
   var material = new THREE.MeshBasicMaterial( { color: color } );
   var cube = new THREE.Mesh( geometry, material );
 
   scene.add( cube );
 
-  var left = random(10) > 5;
-  if(left) {
-    cube.position.x = 0;
-  }
-  else{
-    cube.position.x = window.innerWidth;
-  }
+  winX = window.innerWidth;
+  var posX = random(winX) - (winX / 2);
+  cube.position.x = posX;
+
+  winY = window.innerHeight;
+  var posY = random(winY) - (winY / 2);
+  cube.position.y = posY;
+
+  cube.position.z = -20000
+
   cubes.push(cube);
   return cube;
 }
