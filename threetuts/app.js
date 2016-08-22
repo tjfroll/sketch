0a0ccb72d59c106e2f67a92ef624efeb9dd987a9
Tim Froehlich <tjfroll@gmail.com>
2016-08-22 02:08:58 -0400

    Various mouse event fixes.  Touch support.  Colors.

--- threetuts/app.js
+++ threetuts/app.js
@@ -1,8 +1,9 @@
 var container, stats
 var camera, scene, renderer, particle
-var mouseX = 0, mouseY = 0, mouseDown = false
+var mouseX = 0, mouseY = 0, mouseDown = false, prevMouseX = 0, prevMouseY = 0
 var windowX, windowY
 var windowHalfX, windowHalfY
+var canvasColor = '#772828'
 
 init()
 animate()
@@ -75,7 +76,7 @@ function makeRenderer() {
 }
 
 function setupRenderer() {
-  renderer.setClearColor( 0xEFEFEF )
+  renderer.setClearColor( canvasColor )
   renderer.setPixelRatio( window.devicePixelRatio )
   renderer.setSize( windowX, windowY )
 }
@@ -85,6 +86,7 @@ function setupRenderer() {
 function startEvents() {
   window.addEventListener( 'resize', onWindowResize, false )
   document.addEventListener( 'mousedown', startParticles, false)
+  document.addEventListener( 'touchstart', startParticles, false)
 }
 
 function onWindowResize() {
@@ -99,10 +101,7 @@ function onWindowResize() {
 function restartParticles(e) {
   if (mouseDown) {
     stopParticles()
-    window.requestAnimationFrame(function(frame) {
-      console.log(frame)
-      startParticles(e)
-    })
+    startParticles(e)
   }
 }
 
@@ -110,6 +109,8 @@ function startParticles(e) {
   mouseDown = true
   document.addEventListener( 'mouseup', stopParticles, false)
   document.addEventListener( 'mousemove', restartParticles, false)
+  document.addEventListener( 'touchend', stopParticles, false)
+  document.addEventListener( 'touchmove', restartParticles, false)
   addParticles(e)
 }
 
@@ -117,50 +118,69 @@ function stopParticles(e) {
   mouseDown = false
   document.removeEventListener( 'mouseup', stopParticles, false )
   document.removeEventListener( 'mousemove', restartParticles, false )
+  document.removeEventListener( 'touchend', stopParticles, false)
+  document.removeEventListener( 'touchmove', restartParticles, false)
   document.addEventListener( 'mousedown', startParticles, false)
+  document.addEventListener( 'touchstart', startParticles, false)
 }
 
 // -- particle creation -- //
 
-function addParticles(e) {
+function addParticles(e, currentX, currentY) {
   if (mouseDown) {
+    prevMouseX = mouseX
+    prevMouseY = mouseY
     mouseX = e.clientX - windowHalfX
     mouseY = -e.clientY + windowHalfY
-    addParticle(e)
-    window.requestAnimationFrame(function() { addParticles(e) })
+    var firstSpawn = currentX == null || currentY == null
+    var samePosition = (prevMouseX === currentX && prevMouseY === currentY)
+    if ( firstSpawn || samePosition ) {
+      addParticle(e, getDist())
+      window.requestAnimationFrame(function() {
+        addParticles(e, prevMouseX, prevMouseY)
+      })
+    }
   }
 }
 
-function addParticle(e) {
-  particle = new THREE.Sprite( generateMaterial() )
+function getDist() {
+  var xDist = mouseX - prevMouseX
+  var yDist = mouseY - prevMouseY
+  return Math.floor(Math.sqrt((xDist * xDist) + (yDist * yDist)))
+}
+
+function addParticle(e, dist) {
+  particle = new THREE.Sprite( generateMaterial(dist) )
   initParticle( particle, e )
   scene.add( particle )
 }
 
-function generateMaterial() {
-  var seed = Math.random()
+// -- creating the sprite -- //
+
+function generateMaterial(dist) {
   return new THREE.SpriteMaterial( {
-    map: new THREE.CanvasTexture( generateSprite(seed) ),
-    blending: THREE.SubtractiveBlending
+    map: new THREE.CanvasTexture( generateSprite(dist) ),
+    blending: THREE.AdditiveBlending
   } )
 }
 
-function generateSprite(random) {
+function generateSprite(dist) {
 
   var canvas = document.createElement( 'canvas' )
-  canvas.width = 64
-  canvas.height = 64
   var context = canvas.getContext( '2d' )
-  context.fillStyle = generateGradient(canvas, context, random)
+  context.fillStyle = generateGradient(canvas, context, dist)
   context.fillRect( 0, 0, canvas.width, canvas.height )
 
   return canvas;
 }
 
-function generateGradient(canvas, context, random) {
-  if (random == null) {
-    random = Math.random()
-  }
+function generateGradient(canvas, context, dist) {
+  if (!dist)
+    dist = 1
+  if (dist > 255)
+    dist = 255
+  canvas.width = dist * 4
+  canvas.height = dist * 4
 
   var START_COEFF = 0.5
   var END_COEFF = 0.5
@@ -173,17 +193,34 @@ function generateGradient(canvas, context, random) {
   var r1 = canvas.width * END_COEFF
   var gradient = context.createRadialGradient( x0, y0, r0, x1, y1, r1 )
 
-  var random64 = Math.floor(random * 64)
-  var random75 = Math.floor(random * 75)
-  var random255 = Math.floor(random * 255)
+  console.log(dist)
+
+  var color1 = getColorNumber(dist, 0.75)
+  var color2 = getColorNumber(dist, 1)
+  var color3 = getColorNumber(dist, 1.25)
+  var color4 = getColorNumber(dist, 1.5)
+  var color5 = getColorNumber(dist, 2)
   var stops = [
-    'rgba(' + random255 + ',' + random255 + ', 250,.5)',
-    'rgba(250,' + random75 + ',' + random255 + ', 250)',
-    'rgba(250,250,' + random64 + ',.7)',
-    'rgba(250,250,250,.1)'
+    getRGBString([color2, color1, 0, 0.8]),
+    getRGBString([color3, color1, 0, 0.4]),
+    getRGBString([color5, color1, 0, 0.7]),
+    getRGBString([color2, color1, 0, 0.1]),
+    getRGBString([0, 0, 0, 0])
   ]
+  console.log(stops)
   addStops(gradient, stops)
   return gradient
+}
+
+function getColorNumber(dist, factor) {
+  var num = dist * factor
+  if (num > 255)
+    return 255
+  return Math.floor(num)
+}
+
+function getRGBString(colors) {
+  return 'rgba(' + colors[0] + ',' + colors[1] + ',' + colors[2] + ',' + colors[3] + ')'
 }
 
 function addStops(gradient, stops) {
@@ -192,13 +229,13 @@ function addStops(gradient, stops) {
   }
 }
 
+// -- particle animation and positioning -- //
+
 function initParticle( particle, e ) {
   if (!e)
     return
 
   var particle = this instanceof THREE.Sprite ? this : particle
-
-  // console.log(particle)
 
   particle.position.set( mouseX, mouseY, 0 )
   particle.scale.x = particle.scale.y = Math.random() * 64 + 64
@@ -209,15 +246,15 @@ function initParticle( particle, e ) {
     .start()
 
   var posX = (Math.random() - 0.5) * 5000
-  var posY = (Math.random() - 1) * 5000
+  var posY = (Math.random() - 0.75) * 5000
   var posZ = (Math.random()) * 10000
 
   new TWEEN.Tween( particle.position )
-    .to( { x: posX, y: posY, z: posZ}, 5000 )
+    .to( { x: posX, y: posY, z: posZ}, 15000 )
     .start()
 
   new TWEEN.Tween( particle.scale )
-    .to( { x: .01, y: 20 }, 2000 )
+    .to( { x: .01, y: 20 }, 5000 )
     .start()
 
 }
